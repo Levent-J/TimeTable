faf7fd73a9912e5b4e8027ebae4789c2631991c3
Levent_J <liwenjing960110@gmail.com>
2016-11-19 22:10:58 +0800

    美化课表

---
+++ app/src/main/java/com/levent_j/timetable/activity/CourseSelectActivity.java
@@ -0,0 +1,19 @@
+package com.levent_j.timetable.activity;
+
+import com.levent_j.timetable.R;
+import com.levent_j.timetable.base.BaseActivity;
+
+/**
+ * Created by levent_j on 16-11-19.
+ */
+public class CourseSelectActivity extends BaseActivity{
+    @Override
+    protected void initialize() {
+
+    }
+
+    @Override
+    protected int setLayout() {
+        return R.layout.activity_course_select;
+    }
+}
--- app/src/main/java/com/levent_j/timetable/adapter/TimeTableAdapter.java
+++ app/src/main/java/com/levent_j/timetable/adapter/TimeTableAdapter.java
@@ -1,6 +1,7 @@
 package com.levent_j.timetable.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -8,8 +9,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.levent_j.timetable.R;
+import com.levent_j.timetable.activity.CourseDetailActivity;
 import com.levent_j.timetable.bean.TableCourse;
 
 import java.sql.Time;
@@ -58,6 +61,21 @@ public class TimeTableAdapter extends RecyclerView.Adapter<TimeTableAdapter.mVie
         notifyDataSetChanged();
     }
 
+    public int itemType(int position){
+        //0=时间|1=有课|2=无课
+        if (position%8==0){
+            return 0;
+        }else {
+            int index = position/8+1;
+            TableCourse tableCourse = mTableCourses.get(position-index);
+            if (tableCourse.status==1){
+                return 1;
+            }else {
+                return 2;
+            }
+        }
+    }
+
     class mViewHolder extends RecyclerView.ViewHolder{
         @Bind(R.id.txt)
         TextView textView;
@@ -67,30 +85,57 @@ public class TimeTableAdapter extends RecyclerView.Adapter<TimeTableAdapter.mVie
         public mViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this,itemView);
+
+            backgorund.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    int type = itemType(getLayoutPosition());
+
+                    switch (type){
+                        case 1:
+                            //有课
+                            Intent intentToDetail = new Intent(mContext, CourseDetailActivity.class);
+                            mContext.startActivity(intentToDetail);
+                            break;
+                        case 2:
+                            //无课
+                            //TODO:弹窗提示：是否加入课程？
+                            Intent intentToSelect = new Intent(mContext, CourseDetailActivity.class);
+                            mContext.startActivity(intentToSelect);
+                            break;
+                    }
+                }
+            });
+
+
         }
 
         public void bindViews(int position){
 
-            if (position%8==0){
-                //时间
-                textView.setText("第"+(position%7+(position<50?1:8))+"节课");
-                textView.setTextColor(mContext.getResources().getColor(R.color.colorPrimaryText));
-                backgorund.setBackground(mContext.getResources().getDrawable(colors[0]));
-            }else {
-                int index = position/8+1;
-                TableCourse tableCourse = mTableCourses.get(position-index);
 
-                if (tableCourse.status==1){
+            switch (itemType(position)){
+                case 0:
+                    //时间
+                    textView.setText("第"+(position%7+(position<50?1:8))+"节课");
+                    textView.setTextColor(mContext.getResources().getColor(R.color.colorPrimaryText));
+                    backgorund.setBackground(mContext.getResources().getDrawable(colors[0]));
+                    break;
+                case 1:
                     //有课
+                    int index = position/8+1;
+                    TableCourse tableCourse = mTableCourses.get(position-index);
+
                     textView.setTextColor(mContext.getResources().getColor(R.color.colorWhiteText));
                     backgorund.setBackground(mContext.getResources().getDrawable(colors[tableCourse.cid]));
-                    textView.setText("课程："+tableCourse.cname);
-                }else {
+                    textView.setText(tableCourse.cname+"@"+tableCourse.classroom);
+                    break;
+                case 2:
                     //无课
                     textView.setTextColor(mContext.getResources().getColor(R.color.colorWhiteText));
                     backgorund.setBackground(mContext.getResources().getDrawable(colors[0]));
-                    textView.setText("今日无课");
-                }
+                    textView.setText(" ");
+                    break;
             }
 
         }
