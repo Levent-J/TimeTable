8f5cfab50b6022897167e83103ba90bbc09513ff
Levent_J <liwenjing960110@gmail.com>
2016-11-23 16:41:53 +0800

    about

--- app/src/main/java/com/levent_j/timetable/fragment/AboutFragment.java
+++ app/src/main/java/com/levent_j/timetable/fragment/AboutFragment.java
@@ -1,12 +1,17 @@
 package com.levent_j.timetable.fragment;
 
+import android.widget.TextView;
+
 import com.levent_j.timetable.R;
 import com.levent_j.timetable.base.BaseFragment;
+
+import butterknife.Bind;
 
 /**
  * Created by levent_j on 16-11-9.
  */
 public class AboutFragment extends BaseFragment {
+    @Bind(R.id.tv_about)TextView about;
 
     public static AboutFragment newInstance(){
         return new AboutFragment();
@@ -15,6 +20,12 @@ public class AboutFragment extends BaseFragment {
     @Override
     protected void initialize() {
 
+        String author = "小组成员：李文靖\\王东\\陈文卓";
+        String qq = "联系QQ：609148550";
+
+        String aboutText = author+"\n \n"+qq;
+
+        about.setText(aboutText);
     }
 
     @Override
